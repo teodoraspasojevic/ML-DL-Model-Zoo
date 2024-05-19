66f08addf2a4c353ba4745ec95da54134bffcdad
teodoraspasojevic <spasojevic.teodora@gmail.com>
2024-05-19 19:49:36 +0200

    Updated local surrogate model

--- interpretability/lime.py
+++ interpretability/lime.py
@@ -11,7 +11,8 @@ import torchvision.transforms as transforms
 from PIL import Image
 from skimage.segmentation import slic
 from skimage.color import label2rgb
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import LinearRegression
+from scipy.spatial.distance import cosine
 
 
 class LIME:
@@ -28,7 +29,9 @@ class LIME:
         perturbations (list): List of perturbations applied to the image.
         predictions (np.ndarray): Model predictions for each perturbation.
         top_classes (list): List of top predicted classes for the image.
+        self.similarity_weights(list): List of weights measuring similarity between original and all perturbed images.
         weights (np.ndarray): Weights for each superpixel in the image.
+        self.surrogate_model(sklearn.linear_model): Surrogate model.
         labels (dict): Dictionary mapping class indices to human-readable labels.
     """
 
@@ -45,7 +48,9 @@ class LIME:
         self.perturbations = None
         self.predictions = None
         self.top_classes = None
+        self.similarity_weights = None
         self.weights = None
+        self.surrogate_model = None
 
         with open('./data/imagenet-simple-labels.json') as f:
             self.labels = json.load(f)
@@ -93,11 +98,15 @@ class LIME:
         active_segments = np.unique(segments)
         perturbations = np.random.binomial(1, 0.5, size=(num_perturb, len(active_segments)))
         perturbed_images = np.tile(image, (num_perturb, 1, 1, 1))
+        similarity_weights = []
         for i in range(num_perturb):
             for j, active in enumerate(active_segments):
                 if perturbations[i, j] == 0:
                     perturbed_images[i][segments == active] = 0
+            similarity = 1 - cosine(np.array(image).flatten().reshape(-1,1), perturbed_images[i].flatten().reshape(-1, 1))
+            similarity_weights.append(similarity)
         perturbed_images_pil = [Image.fromarray(img.astype('uint8')) for img in perturbed_images]
+        self.similarity_weights = similarity_weights
         return perturbations, perturbed_images_pil
 
     def transform_image(self, image):
@@ -131,55 +140,6 @@ class LIME:
         probs = torch.nn.functional.softmax(prediction, dim=1)
         top_probs, top_classes = torch.topk(probs, top_n)
         return top_classes.squeeze().tolist()
-
-    def fit_local_surrogate_model(self, perturbations, predictions, top_class_index):
-        """
-        Fits a Ridge regression model to get the local surrogate model weights.
-
-        Args:
-            perturbations (np.ndarray): Array of perturbations.
-            predictions (list): List of model predictions on perturbed images.
-            top_class_index (int): Index of the top class to explain.
-
-        Returns:
-            np.ndarray: local_surrogate weights for the superpixels.
-        """
-        y = np.array([pred.squeeze().tolist()[top_class_index] for pred in predictions])
-        model = Ridge(alpha=1.0)
-        model.fit(perturbations, y)
-        return model.coef_
-
-    def visualize_local_surrogate_model(self, image, segments, weights, i=0):
-        """
-        Visualizes the local surrogate model by only displaying superpixels that influence mostly the prediction.
-
-        Args:
-            image (np.ndarray): Input image.
-            segments (np.ndarray): Segmented image.
-            weights (np.ndarray): local_surrogate weights for the superpixels.
-            i (int): Index for subplot.
-        """
-        # Convert PIL Image to NumPy array.
-        if isinstance(image, Image.Image):
-            image = np.array(image)
-
-        # Identify the top segments with the highest weights.
-        top_segments = np.argsort(weights)[-15:]
-
-        # Create a mask for the top segments.
-        mask = np.isin(segments, top_segments)  # assuming segment labels start from 1
-
-        # Create an all-black image
-        new_image = np.zeros_like(image)
-
-        # Apply the mask to retain original pixels in top segments.
-        new_image[mask] = image[mask]
-
-        # Display the local surrogate model.
-        plt.imshow(new_image)
-        plt.title(f"LIME local_surrogate for top{i+1}-prediction. Prediction label: {self.top_labels[i]}")
-        plt.axis('off')
-        plt.show()
 
     def find_params(self, num_segments=10, num_perturbations=5, top_n=2):
         """
@@ -222,9 +182,59 @@ class LIME:
         plt.title('Perturbed image')
         plt.show()
 
+    def fit_local_surrogate_model(self, perturbations, predictions, top_class_index):
+        """
+        Fits a Linear regression model to get the local surrogate model weights.
+
+        Args:
+            perturbations (np.ndarray): Array of perturbations.
+            predictions (list): List of model predictions on perturbed images.
+            top_class_index (int): Index of the top class to explain.
+
+        Returns:
+            np.ndarray: local_surrogate weights for the superpixels.
+        """
+        y = np.array([pred.squeeze().tolist()[top_class_index] for pred in predictions])
+        weights = np.array(self.similarity_weights)
+        self.surrogate_model = LinearRegression()
+        self.surrogate_model.fit(perturbations, y, sample_weight=weights)
+        return self.surrogate_model.coef_
+
+    def visualize_local_surrogate_model(self, image, segments, weights, i=0):
+        """
+        Visualizes the local surrogate model by only displaying superpixels that influence mostly the prediction.
+
+        Args:
+            image (np.ndarray): Input image.
+            segments (np.ndarray): Segmented image.
+            weights (np.ndarray): local_surrogate weights for the superpixels.
+            i (int): Index for subplot.
+        """
+        # Convert PIL Image to NumPy array.
+        if isinstance(image, Image.Image):
+            image = np.array(image)
+
+        # Identify the top segments with the highest weights.
+        top_segments = np.argsort(weights)[-25:]
+
+        # Create a mask for the top segments.
+        mask = np.isin(segments, top_segments)  # assuming segment labels start from 1
+
+        # Create an all-black image
+        new_image = np.zeros_like(image)
+
+        # Apply the mask to retain original pixels in top segments.
+        new_image[mask] = image[mask]
+
+        # Display the local surrogate model.
+        plt.imshow(new_image)
+        plt.title(f"LIME local_surrogate for top{i+1}-prediction. Prediction label: {self.top_labels[i]}")
+        plt.axis('off')
+        plt.show()
+
     def fit(self, top_n=2):
         """
-        Fits the local_surrogate model for the top N predictions and visualizes the results.
+        Fits the local surrogate model for the top N predictions and visualizes the results.
 
         Args:
             top_n (int): Number of top predictions to explain.
